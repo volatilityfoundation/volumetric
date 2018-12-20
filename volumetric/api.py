323e6b41d246f05fde76ef8b04183a09dfe65ad1
Mike Auty <mike.auty@gmail.com>
2018-12-20 00:00:27 +0000

    Add the VCPL license to volumetric.

--- volumetric/api.py
+++ volumetric/api.py
@@ -1,3 +1,22 @@
+# This file was contributed to the Volatility Framework Version 3.
+# Copyright (C) 2018 Volatility Foundation.
+#
+# THE LICENSED WORK IS PROVIDED UNDER THE TERMS OF THE Volatility Contributors
+# Public License V1.0("LICENSE") AS FIRST COMPLETED BY: Volatility Foundation,
+# Inc. ANY USE, PUBLIC DISPLAY, PUBLIC PERFORMANCE, REPRODUCTION OR DISTRIBUTION
+# OF, OR PREPARATION OF SUBSEQUENT WORKS, DERIVATIVE WORKS OR DERIVED WORKS BASED
+# ON, THE LICENSED WORK CONSTITUTES RECIPIENT'S ACCEPTANCE OF THIS LICENSE AND ITS
+# TERMS, WHETHER OR NOT SUCH RECIPIENT READS THE TERMS OF THE LICENSE. "LICENSED
+# WORK,” “RECIPIENT" AND “DISTRIBUTOR" ARE DEFINED IN THE LICENSE. A COPY OF THE
+# LICENSE IS LOCATED IN THE TEXT FILE ENTITLED "LICENSE.txt" ACCOMPANYING THE
+# CONTENTS OF THIS FILE. IF A COPY OF THE LICENSE DOES NOT ACCOMPANY THIS FILE, A
+# COPY OF THE LICENSE MAY ALSO BE OBTAINED AT THE FOLLOWING WEB SITE:
+# https://www.volatilityfoundation.org/license/vcpl_v1.0
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+# specific language governing rights and limitations under the License.
+#
 import csv
 import hashlib
 import io
@@ -276,7 +295,6 @@ class FileConsumer(interfaces.plugins.FileConsumerInterface):
 
 
 def generate_plugin(automagics, ctx, plugin, plugin_config_path, progress_queue):
-
     def progress_callback(value, message = None):
         progress_queue.put({'type': 'progress', 'data': {'value': value, 'message': message}})
 
@@ -306,9 +324,10 @@ def generate_plugin(automagics, ctx, plugin, plugin_config_path, progress_queue)
 
         progress_queue.put({
             'type':
-            'columns',
+                'columns',
             'data':
-            [(-1, 'depth', 'int')] + [(column.index, column.name, column.type.__name__) for column in result.columns]
+                [(-1, 'depth', 'int')] + [(column.index, column.name, column.type.__name__) for column in
+                                          result.columns]
         })
 
         result.populate(visit, progress_queue)
@@ -381,7 +400,7 @@ class ResultsApi(object):
             return None
         return {
             'size':
-            job['result'].row_count,
+                job['result'].row_count,
             'columns': [{
                 'index': column.index,
                 'name': column.name,
