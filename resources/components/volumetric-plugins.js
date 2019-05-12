c4958079c9b55b87d55fc1b02d9d1d11e26cd1eb
Mike Auty <mike.auty@gmail.com>
2019-05-12 12:12:05 +0100

    Add in plugin filtering.

--- resources/components/volumetric-plugins.js
+++ resources/components/volumetric-plugins.js
@@ -3,6 +3,7 @@ import '/resources/node_modules/paper-collapse-item/paper-collapse-item.js';
 import '/resources/node_modules/@polymer/paper-listbox/paper-listbox.js';
 import '/resources/node_modules/@polymer/paper-item/paper-item.js';
 import '/resources/node_modules/@polymer/paper-card/paper-card.js';
+import '/resources/node_modules/@polymer/paper-input/paper-input.js';
 import '/resources/node_modules/@polymer/iron-ajax/iron-ajax.js';
 import {html} from '../node_modules/@polymer/polymer/lib/utils/html-tag.js';
 
@@ -29,11 +30,14 @@ class VolumetricPlugins extends PolymerElement {
 
 
         <paper-card heading="Available Plugins">
+            <paper-item>
+                <paper-input label="Filter" value="{{list_filter}}"></paper-input>
+            </paper-item>
             <paper-listbox>
-                <template is="dom-repeat" items="[[_top_level(items)]]">
+                <template is="dom-repeat" items="[[_top_level(items)]]" filter="{{_top_level_filter(list_filter, items)}}">
                     <paper-collapse-item header="[[item]]">
                         <paper-listbox>
-                            <template is="dom-repeat" items="[[_filtered(items, item)]]" as="subitem">
+                            <template is="dom-repeat" items="[[_subitems(items, item)]]" filter="{{_plugin_filter(list_filter)}}" as="subitem">
                                 <paper-item value="{{_plugin_name(subitem)}}" on-tap="_select_plugin">
                                     <paper-item-body two-line="">
                                         <div>{{_friendly_name(subitem)}}</div>
@@ -63,6 +67,10 @@ class VolumetricPlugins extends PolymerElement {
             'plugin': {
                 type: String,
                 notify: true
+            },
+            'list_filter': {
+                type: String,
+                notify: true
             }
         }
     }
@@ -75,7 +83,7 @@ class VolumetricPlugins extends PolymerElement {
         return item[0].substring(item[0].lastIndexOf(".") + 1, item[0].length);
     }
 
-    _filtered(items, item) {
+    _subitems(items, item) {
         let result = [];
         for (let i = 0; i < items.length; i++) {
             if (items[i][0].substring(0, item.length) === item) {
@@ -93,6 +101,28 @@ class VolumetricPlugins extends PolymerElement {
 
     _plugin_name(pair) {
         return pair[0];
+    }
+
+    _plugin_filter(list_filter) {
+        return function(pair) {
+            if (pair[0] === null || pair[1] === null) {
+                return true;
+            }
+            return (pair[0].includes(list_filter) || pair[1].includes(list_filter));
+        }
+    }
+
+    _top_level_filter(list_filter, items) {
+        let filt_func = this._plugin_filter(list_filter);
+        return function(item) {
+            let result = false;
+            for (let i = 0; i < items.length; i++) {
+                if (items[i][0].substring(0, items[i][0].indexOf(".")) == item) {
+                    result = result || filt_func(items[i]);
+                }
+            }
+            return result;
+        }
     }
 
     _select_plugin(e) {
